a2b741b2c61ea8352d3e30277134f7889b685073
evi0s <wc810267705@163.com>
2019-07-05 14:10:26 +0800

    Fix bugs & Fix security issues

--- src/app.ts
+++ src/app.ts
@@ -23,16 +23,40 @@ const io = IO(server);
 
 io.on('connection', (socket: any) => {
 
-    socket.on('getData', async (msg: string) => {
-        console.log(msg);
-        let data = await Redis.get('data1');
+    socket.on('getData', async (msg: any) => {
+        console.log(msg.seq);
+        if (! msg.hasOwnProperty("seq") || ! msg.seq) {
+            socket.emit('serverMsg', {"message": "Invalid sequence!"});
+            return;
+        }
+
+        let seq = parseInt(msg.seq).toString();
+        if (seq === "NaN") {
+            socket.emit('serverMsg', {"message": "Invalid sequence!"});
+            return;
+        }
+
+        let data = await Redis.get('data' + seq);
         data = JSON.parse(data);
-        console.log(data);
+        // console.log(data);
         socket.emit('serverMsg', data);
 
     });
 
-    socket.emit('serverMsg', 'fuck');
+    let i = 1;
+    setInterval(async () => {
+        let data = await Redis.get('data' + i);
+        if (! data) {
+            socket.emit('serverMsg', {"message": "No data!"});
+            return;
+        }
+        data = JSON.parse(data);
+        // console.log(data);
+        socket.emit('serverMsg', data);
+        i = i + 1;
+    }, 5000);
+
+    socket.emit('serverMsg', {"message": "fuck"});
 
 });
 
